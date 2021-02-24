4e8b32d49c01178dc96c4bbc41a4fa7d92326663
Daniel <dj.tarsalainen@hotmail.com>
2021-02-24 20:51:16 +0200

    Added a remove function to a school project

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 
-
 function Todolist() {
-  // tuo koko taulun arvoineen. Desc on viimeinen arvo
-  const [item, setItem] = useState({ description: '', date: '' });
-  // luo todo olio, jolla on desc ja date attribuutit
-  // kysyy tehtävän
-  const [todos, setTodos] = useState([]);
-  // näyttää tehtävän¨
-  const [title, setTitle] = useState('');
 
+  const [item, setItem] = useState({ description: '', date: '' });
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState('');
 
   const addTodo = (event) => {
     event.preventDefault();
     setTodos([...todos, item])
-    setTitle(<tr><th>Description</th><th>Title</th></tr>
-    );
+    setTitle(<tr><th>Description</th><th>Title</th></tr>);
   }
 
   const inputChanged = (event) => {
     setItem({ ...item, [event.target.name]: event.target.value });
   }
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
@@ -30,8 +30,8 @@ function Todolist() {
       <div>
         <form onSubmit={addTodo}>
           <fieldset>
-            <legend> Add todo: </legend>
-            <label>Description:</label>
+            <legend>Add todo:</legend>
+            <label>Description: </label>
             <input type="text" name="description" value={item.description} onChange={inputChanged} />
             <label>Date: </label>
             <input type="text" name="date" value={item.date} onChange={inputChanged} />
@@ -42,7 +42,12 @@ function Todolist() {
       <table>
         <tbody>
           {title}
-          {todos.map((item, index) => <tr key={index}><td>{item.date}</td><td>{item.description}</td></tr>)}
+          {todos.map((item, index) =>
+            <tr key={index}>
+              <td>{item.date}</td>
+              <td>{item.description}</td>
+              <td><button value={index} onClick={e => removeTodo(e.target.value)}>Delete</button></td>
+            </tr>)}
         </tbody>
       </table>
     </div>
